98c00a11dfad5bcd9f4cf409e3f890dd8d6e41ff
Joypeterscholar <j.peter@native.semicolon.africa>
2023-10-14 21:17:45 -0400

    Initial commit

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import './App.css';
-import Body from './component/layout/Body';
-
-const App = () => {
-  return <Body />
-}
-
-export default App;
--- src/component/layout/FirstSectionLine1.jsx
+++
@@ -1,9 +0,0 @@
-const FirstSectionLine1=()=>{
-    return (
-      <div className='first_sec_line1'>
-        {/* Control buttons */}
-        {/* ... */}
-      </div>
-    );
-  }
-export default FirstSectionLine1;
